f686f129e9df061235e41401ef891e7da0cef444
Jun <justHungryMan@gmail.com>
2019-09-30 20:45:09 +0900

    [CHANGE] use pickle and time check

--- test1.py
+++ test1.py
@@ -3,7 +3,10 @@ import math
 import pyvista as pv
 import tree as T
 import assignment as AS
+import time
+import pickle
 from tqdm import tqdm_notebook
+from pathlib import Path
 
 from scipy.optimize import linear_sum_assignment
 
@@ -20,14 +23,38 @@ source = teapot_points
 destination = bunny_points * 10
 
 
-Morphing = AS.Assignment(source, destination)
-Morphing.calculate()
-source_points, destination_points = Morphing.get_result()
+
+
+source_points, destination_points = [], []
+src_pts_path = Path("source_pts.pkl")
+dst_pts_path = Path("dst_pts.pkl")
+
+if src_pts_path.exists():
+    with open(src_pts_path, "rb") as fp:
+        source_points = pickle.load(fp)
+    with open(dst_pts_path, "rb") as fp:
+        destination_points = pickle.load(fp)
+
+else :
+    start = time.time()
+    Morphing = AS.Assignment(source, destination)
+    Morphing.calculate()
+    source_points, destination_points = Morphing.get_result()
+    print("time : ", time.time() - start)
+
+
+if not src_pts_path.exists():
+    with open(src_pts_path, "wb") as fp:
+        pickle.dump(source_points, fp)
+
+    with open("dst_pts.pkl", "wb") as fp:
+        pickle.dump(destination_points, fp)
+
 
 
 # Test
 
-FRAME = 600
+FRAME = 240
 filename = "test.mp4"
 
 
@@ -44,7 +71,9 @@ destination_dataset = pv.PolyData(np.array(destination_points))
 
 plotter = pv.Plotter()
 plotter.open_movie(filename)
-plotter.add_mesh(start_dataset, color='red')
+plotter.set_position([-1, 2, -5])
+plotter.enable_eye_dome_lighting()
+plotter.add_mesh(start_dataset, color='red', show_edges = True)
 
 plotter.show(auto_close = False)
 plotter.write_frame()
